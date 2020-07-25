0c85c8f5d44e73b27b09afa7f52dfdfdeeb33e3e
LuanSSouza <luan.souza27@hotmail.com>
2020-07-24 23:52:38 -0300

    Implementando Layout da tela de recomendacoes

--- src/pages/ExplanationRate/ExplanationRate.jsx
+++ src/pages/ExplanationRate/ExplanationRate.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
+import { Jumbotron, Button, Row, Col, Image } from 'react-bootstrap';
+import testeImg from './teste.jpg';
 
 export class ExplanationRate extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { movie: { justify: "xyz"}, rate: {} };
+        this.state = { 
+            movies: [ 
+                { title: "Images (1972)", justify: "Schizophrenic housewife, engulfed by terrorizing apparitions, kills off each, unknowing if these demons are merely figments of her hallucinatory imagination or part of reality." },
+                { title: "Images (1972)", justify: "Schizophrenic housewife, engulfed by terrorizing apparitions, kills off each, unknowing if these demons are merely figments of her hallucinatory imagination or part of reality." },
+                { title: "Images (1972)", justify: "Schizophrenic housewife, engulfed by terrorizing apparitions, kills off each, unknowing if these demons are merely figments of her hallucinatory imagination or part of reality." }
+            ]
+            , rate: {} 
+        };
 
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handlerNext = this.handlerNext.bind(this);
     }
 
-    handleSubmit(event) {
+    handlerNext(event) {
         event.preventDefault();
 
         this.props.history.push('/explanationCompare');
@@ -18,14 +27,23 @@ export class ExplanationRate extends Component {
     render() {
         return (
             <div>
-                <div> { this.state.movie.justify } </div>
-                <br/>
-                <div>
-                    <form onSubmit={this.handleSubmit}>
-                        {/* TODO - Verify our avaliations parameters */}
-                        <input type="submit" value="Next"/>
-                    </form>
-                </div>
+                {
+                    this.state.movies.map( (movie) => (
+                        <Jumbotron>
+                            <Row className="mb-4">
+                                <Col xs={12} md={3} className="text-center">
+                                    <Image src={testeImg} className="m-2"/>
+                                </Col>
+                                <Col xs={12} md={9}>
+                                    <h1>{ movie.title }</h1>
+                                    <p> { movie.justify } </p>
+                                </Col>
+                            </Row>
+                            <Button variant="primary" className="float-md-right">Rate</Button>
+                        </Jumbotron>
+                    ))
+                }
+                <Button variant="primary" className="float-md-right" onClick={this.handlerNext}>Next</Button>
             </div>
         )
     }
