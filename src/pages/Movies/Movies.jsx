02905d3ccdd402864778d7cfb208b0fa8aa5033e
Andre Zanon <andrelzanon10@gmail.com>
2020-07-26 20:53:19 -0300

    Inicio de implementacao de perfil do usuario

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -5,7 +5,12 @@ import ImageList from '../../components/ImageList';
 
 export class Movies extends Component {
 
-    state = {films: []}
+    state = {films: [], profile_films: []};
+
+    constructor(props){
+        super(props)
+        this.handler = this.handler.bind(this)
+    }
 
     onSearchSubmit = async (film) => {
         const response = await Omdb.get( '', {
@@ -14,15 +19,28 @@ export class Movies extends Component {
             }
         });
 
-        console.log(response.data.Search)
+        // console.log(response.data.Search)
         this.setState({films: response.data.Search})
+    }
+
+    /**
+     * Add a new movie to the profile itens, in case it was not yet added
+     * 
+     * @param {*} Title to be addded to the profile_itens
+     */
+    handler = (Title) => {
+        if(!this.state.profile_films.includes(Title)){
+            this.setState({
+                profile_films:[...this.state.profile_films, Title]
+            });
+        }
     }
 
     render() {
         return (
             <div>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
-                <ImageList films={this.state.films}/>
+                <ImageList handler = {this.handler} films={this.state.films}/>
             </div>
         )
     }
