5a37d111bc7948aea00a58f4b1514ae5289c6cd9
Andre Zanon <andrelzanon10@gmail.com>
2020-07-27 14:33:11 -0300

    Implementação de adicao na lista de perfil do usuario

---
+++ src/components/ProfileList.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Button,Modal } from 'react-bootstrap'
+import './profilelist.css';
+
+class ProfileList extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            showHide : false
+        }
+    }
+
+    handleModalShowHide() {
+        this.setState({ showHide: !this.state.showHide })
+    }
+
+    render(){
+        return(
+            <div>
+                <Button id="scrollup-btn" variant="primary" onClick={() => this.handleModalShowHide()}>
+                    Profile List 
+                </Button>
+
+                <Modal  
+                        size="lg"
+                        aria-labelledby="contained-modal-title-vcenter"
+                        centered 
+                        show={this.state.showHide}>
+                    <Modal.Header closeButton onClick={() => this.handleModalShowHide()}>
+                        <Modal.Title id="m-title">Films That You Liked</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {this.props.pro_list.map(item => {
+                            return  <div className="row" key={item.imdbID}>
+                                        <div className="column col-lg-3  center">
+                                            <img className="pro-film-poster" src={item.Poster} alt={"Poster of the film " + item.Title + " from the year " + item.Year} />
+                                        </div>
+                                        <div className="column col-lg-5 center">
+                                            <h1 className="pro-film-title">{item.Title}</h1>
+                                        </div>
+                                        <div className="column col-lg-2 center">
+                                            <h1 className="pro-film-year">{item.Year}</h1>
+                                        </div>
+                                        <div className="column col-lg-2 center">
+                                            <button type="button" className="btn text-danger">
+                                            <svg viewBox="0 0 16 16" class="bi bi-trash-fill trash-icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+                                            </svg>
+                                            </button>
+                                        </div>
+                                    </div>
+                        })}
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button className="btn-danger" variant="secondary" onClick={() => this.handleModalShowHide()}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
+            </div>
+        )
+    }
+    
+}
+
+export default ProfileList;
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 import Omdb from '../../services/Omdb'
 import SearchBar from '../../components/SearchBar';
 import ImageList from '../../components/ImageList';
+import ProfileList from '../../components/ProfileList';
 
 export class Movies extends Component {
 
     state = {films: [], profile_films: []};
 
     constructor(props){
-        super(props)
-        this.handler = this.handler.bind(this)
+        super(props);
+        this.findFilmByImdbId = this.findFilmByImdbId.bind(this);
+        this.handler = this.handler.bind(this);
     }
 
     onSearchSubmit = async (film) => {
@@ -23,15 +25,32 @@ export class Movies extends Component {
         this.setState({films: response.data.Search})
     }
 
+    findFilmByImdbId = (imbd_id) => {
+        for(var i = 0; i < this.state.profile_films.length; i++){
+            const f = this.state.profile_films[i];
+            if(f.imdbID === imbd_id){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
     /**
      * Add a new movie to the profile itens, in case it was not yet added
      * 
      * @param {*} Title to be addded to the profile_itens
      */
-    handler = (Title) => {
-        if(!this.state.profile_films.includes(Title)){
+    handler = (imbd_id, url, title, year) => {
+        if(!this.findFilmByImdbId(imbd_id)){
+            var new_film = {
+                imdbID: imbd_id,
+                Poster: url,
+                Title: title,
+                Year: year
+            }
             this.setState({
-                profile_films:[...this.state.profile_films, Title]
+                profile_films:[...this.state.profile_films, new_film]
             });
         }
     }
@@ -39,6 +58,7 @@ export class Movies extends Component {
     render() {
         return (
             <div>
+                <ProfileList pro_list={this.state.profile_films}></ProfileList>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
                 <ImageList handler = {this.handler} films={this.state.films}/>
             </div>
