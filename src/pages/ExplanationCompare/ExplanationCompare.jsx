f9d1242fc43ceec742e6e3455fe25b8e3d8b71bb
LuanSSouza <luan.souza27@hotmail.com>
2020-07-27 23:21:04 -0300

    implementacao da comparacao das justificativas

--- src/pages/ExplanationCompare/ExplanationCompare.jsx
+++ src/pages/ExplanationCompare/ExplanationCompare.jsx
@@ -1,62 +1,149 @@
 import React, { Component } from 'react'
-import { Form, Button } from 'react-bootstrap';
+import { Jumbotron, Form, Button, Row, Col, Image } from 'react-bootstrap';
+import testeImg from '../ExplanationRate/teste.jpg';
 
 export class ExplanationCompare extends Component {
     constructor(props) {
         super(props);
 
         this.state = { 
-            movie: { justifyA: "I'm the justification A", justifyB: "I'm the justification B" }, 
-            prefer: 0, 
-            clareza: 0, 
-            confianca: 0, 
-            descoberta: 0
+            movies: [ 
+                { 
+                    justifyA: "Schizophrenic housewife, engulfed by terrorizing apparitions, kills off each, unknowing if these demons are merely figments of her hallucinatory imagination or part of reality.", 
+                    justifyB: "Schizophrenic housewife, engulfed by terrorizing apparitions, kills off each, unknowing if these demons are merely figments of her hallucinatory imagination or part of reality.", 
+                    title: "Images (1972)",
+                    rec: 0, 
+                    qualiA: 0, 
+                    qualiB: 0, 
+                    wordsA: 0, 
+                    wordsB: 0,
+                    recComments: "",
+                    jusComments: "", 
+                },
+                { 
+                    justifyA: "Schizophrenic housewife, engulfed by terrorizing apparitions, kills off each, unknowing if these demons are merely figments of her hallucinatory imagination or part of reality.", 
+                    justifyB: "Schizophrenic housewife, engulfed by terrorizing apparitions, kills off each, unknowing if these demons are merely figments of her hallucinatory imagination or part of reality.", 
+                    title: "Images (1972)",
+                    rec: 0, 
+                    qualiA: 0, 
+                    qualiB: 0, 
+                    wordsA: 0, 
+                    wordsB: 0,
+                    recComments: "",
+                    jusComments: "", 
+                },
+            ], 
         };
     }
 
-    handleChangePrefer = (event) => this.setState({ prefer: event.target.value });
+    handleChangeRec = (event, index) => {
+        let aux = this.state.movies;
+        aux[index].rec = event.target.value;
+        this.setState({ movies: aux  });
+    }
 
-    handleChangeClareza = (event) => this.setState({ clareza: event.target.value });
+    handleChangeQualiA = (event, index) => {
+        let aux = this.state.movies;
+        aux[index].qualiA = event.target.value;
+        this.setState({ movies: aux  });
+    }
 
-    handleChangeConfianca = (event) => this.setState({ confianca: event.target.value });
+    handleChangeQualiB = (event, index) => {
+        let aux = this.state.movies;
+        aux[index].qualiB = event.target.value;
+        this.setState({ movies: aux  });
+    }
 
-    handleChangeDescoberta = (event) => this.setState({ descoberta: event.target.value });
+    handleChangeWordsA = (event, index) => {
+        let aux = this.state.movies;
+        aux[index].wordsA = event.target.value;
+        this.setState({ movies: aux  });
+    }
 
-    handleSubmit = (event) => {
+    handleChangeWordsB = (event, index) => {
+        let aux = this.state.movies;
+        aux[index].wordsB = event.target.value;
+        this.setState({ movies: aux  });
+    }
+
+    handleChangeJusComments = (event, index) => {
+        let aux = this.state.movies;
+        aux[index].jusComments = event.target.value;
+        this.setState({ movies: aux  });
+    }
+
+    handleChangeRecComments = (event, index) => {
+        let aux = this.state.movies;
+        aux[index].recComments = event.target.value;
+        this.setState({ movies: aux  });
+    }
+
+    handlerFinish = (event) => {
         event.preventDefault();
-        console.log(this.state.rate)
         this.props.history.push('/');
     }
 
     render() {
         return (
-            <Form onSubmit={this.handleSubmit}>
-                <Form.Group controlId="prefer">
-                    <Form.Label className="float-left">Jutification A</Form.Label>
-                    <Form.Label className="float-right">Jutification B</Form.Label>
-                    <Form.Control type="range"  min="-10" max="10" value={this.state.prefer} onChange={this.handleChangePrefer} />
-                </Form.Group>
+            <div>
+                {
+                    this.state.movies.map( (movie, index) => (
+                        <Jumbotron key={index}>
+                            <Row className="mb-4">
+                                <Col xs={12} md={4}>
+                                    <h2><b>Justification A:</b></h2>
+                                    <p> { movie.justifyA } </p>
+                                </Col>
+                                <Col xs={12} md={4} className="text-center">
+                                    <h2><b> { movie.title } </b></h2>
+                                    <Image src={testeImg} className="m-2"/>
+                                </Col>
+                                <Col xs={12} md={4}>
+                                    <h2><b>Justification B:</b></h2>
+                                    <p> { movie.justifyB } </p>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={12} md={6}>
+                                    <Form.Group controlId="rec">
+                                        <Form.Label>Recommendation quality:</Form.Label>
+                                        <Form.Control type="range"  min="0" max="10" value={movie.rec} onChange={ (e)=> this.handleChangeRec(e, index)} />
+                                    </Form.Group>
+                                    <Form.Group controlId="qualiA">
+                                        <Form.Label>Quality of justification A:</Form.Label>
+                                        <Form.Control type="range"  min="0" max="10" value={movie.qualiA} onChange={ (e)=> this.handleChangeQualiA(e, index)} />
+                                    </Form.Group>
 
-                <Form.Group controlId="clareza">
-                    <Form.Label className="float-left">Jutification A</Form.Label>
-                    <Form.Label className="float-right">Jutification B</Form.Label>
-                    <Form.Control type="range"  min="-10" max="10" value={this.state.clareza} onChange={this.handleChangeClareza} />
-                </Form.Group>
+                                    <Form.Group controlId="qualiB">
+                                        <Form.Label>Quality of justification B:</Form.Label>
+                                        <Form.Control type="range"  min="0" max="10" value={movie.qualiB} onChange={ (e)=> this.handleChangeQualiB(e, index)} />
+                                    </Form.Group>
 
-                <Form.Group controlId="confianca">
-                    <Form.Label className="float-left">Jutification A</Form.Label>
-                    <Form.Label className="float-right">Jutification B</Form.Label>
-                    <Form.Control type="range"  min="-10" max="10" value={this.state.confianca} onChange={this.handleChangeConfianca} />
-                </Form.Group>
-
-                <Form.Group controlId="descoberta">
-                    <Form.Label className="float-left">Jutification A</Form.Label>
-                    <Form.Label className="float-right">Jutification B</Form.Label>
-                    <Form.Control type="range"  min="-10" max="10" value={this.state.descoberta} onChange={this.handleChangeDescoberta} />
-                </Form.Group>
-
-                <Button className="float-lg-right" as="input" type="submit" value="Finish" />
-            </Form>
+                                    <Form.Group controlId="wordsA">
+                                        <Form.Label>Quality of the words of justification A:</Form.Label>
+                                        <Form.Control type="range"  min="0" max="10" value={movie.wordsA} onChange={ (e)=> this.handleChangeWordsA(e, index)} />
+                                    </Form.Group>
+                                    <Form.Group controlId="wordsB">
+                                        <Form.Label>Quality of the words of justification B:</Form.Label>
+                                        <Form.Control type="range"  min="0" max="10" value={movie.wordsB} onChange={ (e)=> this.handleChangeWordsB(e, index)} />
+                                    </Form.Group>
+                                </Col>
+                                <Col xs={12} md={6}>
+                                    <Form.Group controlId="recComments">
+                                        <Form.Label>Comments on the Recommendation:</Form.Label>
+                                        <Form.Control as="textarea" rows="4" value={movie.recComments} onChange={ (e)=> this.handleChangeRecComments(e, index)} />
+                                    </Form.Group>
+                                    <Form.Group controlId="jusComments">
+                                        <Form.Label>Comments on the Justification:</Form.Label>
+                                        <Form.Control as="textarea" rows="4" value={movie.jusComments} onChange={ (e)=> this.handleChangeJusComments(e, index)} />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                        </Jumbotron>
+                    ))
+                }
+                <Button variant="primary" className="float-md-right" onClick={this.handlerFinish}>Finish</Button>
+            </div>
         )
     }
 }
